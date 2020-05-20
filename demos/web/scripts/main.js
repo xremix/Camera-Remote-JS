7fbedffdb61074494f7020f994e7f9bce10d26b8
xremix <tonidhoffmann@gmail.com>
2020-05-20 10:01:48 +0200

    adding selfie functionality to the demos

--- demos/web/scripts/main.js
+++ demos/web/scripts/main.js
@@ -1,11 +1,27 @@
 $(function() {
-  console.log(Camera);
+	function consolelog(t){
+		$('#debugOutput').text($('#debugOutput').text() + "\n" + JSON.stringify(t));
+	}
+
+	function showImageFromResponse(resp){
+		try {
+			// var resp = {"result":[["http://192.168.122.1:8080/postview/pict20200520_080827_0.JPG"]],"id":1};
+			if(resp.result){
+				var path = resp.result[0][0];
+				$('#previewimage').attr('src', path);
+			}
+		} catch (e) {
+
+		}
+	}
+
+  consolelog(Camera);
   $('#initCamerButton').on('click', function(e) {
     e.preventDefault();
     Camera.GetVersion(function(resp) {
-      console.log(resp);
+      consolelog(resp);
       Camera.StartRecMode(function(_resp) {
-        console.log(_resp);
+        consolelog(_resp);
       });
     });
   });
@@ -13,61 +29,108 @@ $(function() {
   $('#getEventButton').on('click', function(e) {
     e.preventDefault();
     Camera.GetEvent(function(resp) {
-      console.log(resp);
-      $('#debugOutput').text(JSON.stringify(resp));
+      consolelog(resp);
+      consolelog(JSON.stringify(resp));
     });
   });
 
   $('#startLiveviewBtn').on('click', function(e) {
     e.preventDefault();
     Camera.StartLiveview(function(resp) {
-      console.log(resp);
+      consolelog(resp);
     });
   });
 
   $('#stopLiveviewBtn').on('click', function(e) {
     e.preventDefault();
-    console.log(Camera);
+    consolelog(Camera);
     Camera.StopLiveview(function(resp) {
-      console.log(resp);
+      consolelog(resp);
     });
   });
 
   $('#bodyactTakePictureBtn').on('click', function(e) {
     e.preventDefault();
     Camera.ActTakePicture(function(resp) {
-      console.log(resp);
+      consolelog(resp);
+			showImageFromResponse(resp);
     });
   });
 
   $('#bodyactTakeSelfieBtn').on('click', function(e) {
     e.preventDefault();
-    var amount = document.getElementById("numberOfSelfies").value;
 
-    function takeselfie(amount) {
-      if (amount !== 0) {
-        Camera.ActTakePicture(function(resp) {
-          console.log(resp);
-          takeselfie(--amount);
-        });
-      } else {
-        alert("Done");
-      }
-      takeselfie(amount);
-    };
+    var amount = document.getElementById("numberOfSelfies").value;
+    var timeout = document.getElementById("selfieDelay").value * 1000;
+    var backupSelfie = document.getElementById("backupSelfie").value;
+		consolelog("Selfie time");
+		consolelog("amount:");
+		consolelog(amount);
+		consolelog("timeout:");
+		consolelog(timeout);
+		consolelog("backupSelfie:");
+		consolelog(backupSelfie);
+
+		function takeselfie(amount) {
+			consolelog("Taking selfie after delay");
+			if (amount !== 0) {
+				setTimeout(function(){
+					consolelog("Selfie starting");
+					Camera.ActTakePicture(function(resp) {
+						consolelog("Selfie done, " + amount + " selfies left");
+						consolelog(resp);
+						showImageFromResponse(resp);
+						takeselfie(--amount);
+					});
+				}, timeout)
+			} else {
+				alert("Done");
+			}
+		}
+
+		function backupselfie(amount) {
+			consolelog("Starting backup selfie (which has a different delay method)");
+			function myLoop() {
+				setTimeout(function() {
+					consolelog("Taking picture and waiting for response");
+					//////////
+					Camera.ActTakePicture(function(resp) {
+						consolelog("Selfie done, " + amount + " selfies left");
+						consolelog(resp);
+						showImageFromResponse(resp);
+						takeselfie(--amount);
+					});
+					//////////
+					if (--amount !== 0) {
+						myLoop();
+					}else{
+						alert("done")
+					}
+				}, timeout)
+			}
+
+			myLoop();
+		}
+		if(backupSelfie == true || backupSelfie == 'on' || backupSelfie == 'yes'){
+			backupselfie(amount);
+		}else{
+		takeselfie(amount);
+		}
+
   });
 
   $('#bodystartBulbShootingBtn').on('click', function(e) {
     e.preventDefault();
     Camera.StartBulbShooting(function(resp) {
       startBulbStopWatch();
-      console.log(resp);
+      consolelog(resp);
     });
   });
   $('#bodystopBulbShootingBtn').on('click', function(e) {
     e.preventDefault();
     Camera.StopBulbShooting(function(resp) {
-      console.log(resp);
+      consolelog(resp);
+			showImageFromResponse(resp);
       stopBulbStopWatch();
     });
   });
@@ -81,7 +144,7 @@ $(function() {
       var timeDifference = (_date - new Date());
       var secondsDifference = Math.round(timeDifference / 1000) * -1;
       $('#stopWatchValue').text(secondsDifference + "s");
-      console.log(secondsDifference);
+      consolelog(secondsDifference);
     }, 1000);
   }
 
